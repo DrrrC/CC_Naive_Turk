f1067a408ce72c4230e1657f2001cddf76cb42c2
bobbyhyz <bobbyhyz@gmail.com>
2017-12-01 18:28:01 -0500

    updated NT.js

--- NT.js
+++ NT.js
@@ -2,7 +2,33 @@
 
 // contributor : Yuntao Zhou, Yanzun Huang, Chuankai Zhang
 
+function clickStore(e) {
+  // chrome.tabs.executeScript(null,
+  //     {code:"window.open('https://www.google.com', '_blank')"});
+    chrome.tabs.create({url: chrome.extension.getURL('store.html')});
+    window.close();
+}
 
+function clickSearch(e) {
+    // chrome.tabs.executeScript(null,
+    //     {code:"window.open('https://www.bing.com', '_blank')"});
+    chrome.tabs.create({url: chrome.extension.getURL('search.html')});
+    window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var divs = document.querySelectorAll('div');
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].id == "store")
+        {
+            divs[i].addEventListener('click', clickStore);
+        }
+        else
+        {
+            divs[i].addEventListener('click', clickSearch);
+        }
+    }
+});
 
 var globle_fs;
 
