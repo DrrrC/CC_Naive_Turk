881ca61bfefe2a2c14d7dc7ba050df84692f6b28
bobbyhyz <bobbyhyz@gmail.com>
2017-12-02 17:48:58 -0500

    added append list in store.js

--- store.js
+++ store.js
@@ -6,6 +6,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
 
 function storeContent(e) {
   console.log(document.getElementById('title').value);
+  append_file("list.txt", document.getElementById('title').value + '\n');
   write_file(document.getElementById('title').value + '_content.txt', document.getElementById('actualText').value);
 }
 
