ea1d66ac66e91ed22fc491689cda1518d1c1f12c
zyt <yuntaoz@andrew.cmu.edu>
2017-12-02 16:21:29 -0500

    all the api for filesystem functional

--- file_opt.js
+++ file_opt.js
@@ -4,23 +4,123 @@
 
 // file operation javascript file
 
+var read_list,write_file,load_content,append_content,delete_file;
 
-var globle_fs;
-
-function onInitFs(fs) {
-  console.log('Opened file system: ' + fs.name);
+var onInitFs = function(fs) {
+  //console.log('Opened file system: ' + fs.name);
   fs.root.getFile('list.txt', {create: true, exclusive: true}, function(fileEntry) {
   	// the list file contains all the title:keywords,keywords,keywords ,
   	// one save one line
 
   }, errorHandler);
 
-  globle_fs = fs;
-  console.log(globle_fs);
-  write_file("test.txt","blabla");
-  read_file("test.txt");
+  // read file helping function ********************
+  read_list = function(filename){
+    fs.root.getFile(filename, {}, function(fileEntry) {
+
+      // Get a File object representing the file,
+      // then use FileReader to read its contents.
+      fileEntry.file(function(file) {
+         var reader = new FileReader();
+         
+         reader.onloadend = function(e) {
+           // return the readcontent
+           console.log(this.result);
+         };
+
+         reader.readAsText(file);
+      }, errorHandler);
+
+    }, errorHandler);
+
+  }
+  // write certain content to a file with filename given
+  write_file = function(filename, content){
+    console.log("in");
+    fs.root.getFile(filename, {create: true}, function(fileEntry) {
+
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function(fileWriter) {
+
+      fileWriter.onwriteend = function(e) {
+        console.log('Write completed.');
+      };
+
+      fileWriter.onerror = function(e) {
+        console.log('Write failed: ' + e.toString());
+      };
+
+      // Create a new Blob and write it to log.txt.
+      var blob = new Blob([content], {type: 'text/plain'});
+
+      fileWriter.write(blob);
+
+    }, errorHandler);
+
+  }, errorHandler);
+  }
 
   
+  // load the content of certain file and add a textarea element to the html
+  load_content = function(filename){
+    fs.root.getFile(filename, {}, function(fileEntry) {
+
+      // Get a File object representing the file,
+      // then use FileReader to read its contents.
+      fileEntry.file(function(file) {
+         var reader = new FileReader();
+         reader.onloadend = function(e) {
+           // return the readcontent
+           var txtArea = document.createElement('textarea');
+           txtArea.value = this.result;
+           $("#content_board").append(txtArea);
+           console.log(this.result);
+         };
+
+         reader.readAsText(file);
+      }, errorHandler);
+
+    }, errorHandler);
+
+  }
+
+  // append certain content to certain file
+  append_content = function(filename,content){
+    fs.root.getFile(filename, {create: false}, function(fileEntry) {
+
+      // Create a FileWriter object for our FileEntry (log.txt).
+      fileEntry.createWriter(function(fileWriter) {
+
+        fileWriter.seek(fileWriter.length); // Start write position at EOF.
+
+        // Create a new Blob and write it to log.txt.
+        var blob = new Blob([content], {type: 'text/plain'});
+
+        fileWriter.write(blob);
+
+      }, errorHandler);
+
+    }, errorHandler);
+
+
+  }
+
+// delete certain file
+  delete_file = function(filename){
+    fs.root.getFile(filename, {create: false}, function(fileEntry) {
+
+      fileEntry.remove(function() {
+        console.log('File removed.');
+      }, errorHandler);
+
+    }, errorHandler);
+
+  }
+ 
+ // prepare the list elements for the search group *******************
+
+ // call read_list to get all existing saved survey titles
+ 
 
 
 }
@@ -55,89 +155,15 @@ function errorHandler(e) {
 // request the persistent storage space from local from user
 navigator.webkitPersistentStorage.requestQuota( 50*1024*1024 /*50MB*/, function(grantedBytes) {
   window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
-  window.requestFileSystem(window.PERSISTENT, grantedBytes , onInitFs,errorHandler);
+  window.requestFileSystem(window.PERSISTENT, grantedBytes , onInitFs, errorHandler);
 }, function(e) {
   console.log('Error', e);
 });
 
 
-// read file helping function ********************
-function read_file(filename){
-	globle_fs.root.getFile(filename, {}, function(fileEntry) {
-
-    // Get a File object representing the file,
-    // then use FileReader to read its contents.
-    fileEntry.file(function(file) {
-       var reader = new FileReader();
-       console.log("here");
-       reader.onloadend = function(e) {
-         // return the readcontent
-         console.log(this.result);
-       };
-
-       reader.readAsText(file);
-    }, errorHandler);
-
-  }, errorHandler);
-
-}
-
-// write content to certain file name file
-function write_file(filename, content){
-	globle_fs.root.getFile(filename, {create: true}, function(fileEntry) {
-
-    // Create a FileWriter object for our FileEntry (log.txt).
-    fileEntry.createWriter(function(fileWriter) {
-
-      fileWriter.onwriteend = function(e) {
-        console.log('Write completed.');
-      };
-
-      fileWriter.onerror = function(e) {
-        console.log('Write failed: ' + e.toString());
-      };
-
-      // Create a new Blob and write it to log.txt.
-      var blob = new Blob([content], {type: 'text/plain'});
-
-      fileWriter.write(blob);
-
-    }, errorHandler);
-
-  }, errorHandler);
-}
-
-// append certain content to certain file
-function append_content(filename,content){
-	globle_fs.root.getFile(filename, {create: false}, function(fileEntry) {
-
-    // Create a FileWriter object for our FileEntry (log.txt).
-    fileEntry.createWriter(function(fileWriter) {
-
-      fileWriter.seek(fileWriter.length); // Start write position at EOF.
-
-      // Create a new Blob and write it to log.txt.
-      var blob = new Blob([content], {type: 'text/plain'});
-
-      fileWriter.write(blob);
-
-    }, errorHandler);
-
-  }, errorHandler);
 
 
-}
 
-// delete certain file
-function delete_file(filename){
-	globle_fs.root.getFile(filename, {create: false}, function(fileEntry) {
 
-    fileEntry.remove(function() {
-      console.log('File removed.');
-    }, errorHandler);
-
-  }, errorHandler);
-
-}
 
 
--- search.js
+++ search.js
@@ -33,3 +33,7 @@ $(function() {
         $('#groupid').slideUp()
     });
 })
+
+
+
+
